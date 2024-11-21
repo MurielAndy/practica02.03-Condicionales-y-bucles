70581dc3e1b092df1803c1a21b9d8a4839509acc
MurielAndy <amurielcab@educacion.navarra.es>
2024-11-21 23:59:27 +0100

    .

---
+++ Ejercicio1.py
@@ -0,0 +1,7 @@
+# Contraseña
+contraseña_guardada = "contraseña"
+contraseña_usuario = input("Introduce la contraseña: ")
+if contraseña_guardada.lower() == contraseña_usuario.lower():
+    print("La contraseña es correcta.")
+else:
+    print("La contraseña es incorrecta.")
